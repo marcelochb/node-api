bb2ef215cac92ad973c4b0e2d6b819fc2baec8c0
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-07-10 22:34:43 -0300

    curso de nodeapi

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Iniciando App
+const app = express();
+
+// Iniciando o DB
+mongoose.connect("mongodb://localhost:27012/nodeapi", {useNewUrlParser: true});
+
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
+
+// Primeira rota
+app.get("/", (req,res) => {
+     Product.create({
+         title: "React Native",
+         description: "Build native apps with React",
+         url: "http://github.com/facebook/react-native"
+     });
+     return res.send("Hello Rockseat");
+});
+
+app.listen(3001);
